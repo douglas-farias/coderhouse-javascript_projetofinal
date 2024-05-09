73c660b2462766e154f6ddcca0b33a8dba5fab0c
douglas-farias <douglas.vfarias@gmail.com>
2024-05-09 16:56:46 -0300

    Novos arquivos html e css

---
+++ scripts/cadastro-usuario.js
@@ -0,0 +1,11 @@
+class usuarioDados {
+        constructor (nome, sobrenome, logradouroEndereco, numeroEndereco, cepEndereco, usuario, senha) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.logradouroEndereco = logradouroEndereco;
+        this.numeroEndereco = numeroEndereco,
+        this.cepEndereco = cepEndereco;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+}
