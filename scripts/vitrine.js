1709edbc6395c3663ecc35a3c032258de07735f6
douglas-farias <douglas.vfarias@gmail.com>
2024-05-16 22:13:22 -0300

    atualizacao dos scripts index, vitrine e listaProdutos

--- scripts/vitrine.js
+++ scripts/vitrine.js
@@ -4,14 +4,14 @@ function renderizarProdutos(filtro, valor) {
     const tituloFiltro = document.getElementById("conteudo__titulo");
     tituloFiltro.innerText = "";
 
-    const container = document.getElementsByClassName("conteudo__vitrine");
+    const container = document.getElementsByClassName("conteudo__vitrine")[0];
     container.innerHTML = "";
 
     let produtosFiltrados = [];
 
     if (filtro === "categoria") {
         tituloFiltro.innerText = `Categoria ${valor.charAt(valor.length - 1).toUpperCase()}`;
-        produtosFiltrados = produtos[valor];
+        produtosFiltrados = produtosCadastrados[valor];
     } else if (filtro === "novidades") {
         tituloFiltro.innerText = "Novidades";
         produtosFiltrados = Object.values(produtosCadastrados).flat().filter(produto => produto.novidade);
@@ -19,4 +19,34 @@ function renderizarProdutos(filtro, valor) {
         tituloFiltro.innerText = "Ofertas";
         produtosFiltrados = Object.values(produtosCadastrados).flat().filter(produto => produto.oferta);
     }
-}
+
+    produtosFiltrados.forEach(produto => {
+        const divProduto = document.createElement("div");
+        divProduto.className = "conteudo__produto";
+        divProduto.id = `prodtuo__${produto.id}`;
+
+        divProduto.innerHTML = `<a href="produto.html?id=${produto.id}">
+                                    <img src="${produto.imagem}" alt="${produto.imagemAlt}">
+                                </a>
+                                <div>
+                                    <h4>${produto.nome}</h4>
+                                    <span id="precoProduto${produto.id}">R$ ${produto.precoString}</span>
+                                </div>`;
+
+        container.appendChild(divProduto)
+    });
+
+    
+};
+
+const urlParams = new URLSearchParams(window.location.search);
+const categoria = urlParams.get('categoria');
+const filtro = urlParams.get('filtro');
+
+if (categoria) {
+    renderizarProdutos('categoria', categoria);
+} else if (filtro) {
+    renderizarProdutos(filtro);
+};
+
+export { renderizarProdutos }
